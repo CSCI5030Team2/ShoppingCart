da12534bbc44278c76f111e56ee6b0abb87789ef
Bohan Zhang <bohan.zhang.1@slu.edu>
2022-09-29 07:17:13 -0500

    Add: User module

---
+++ springServer/src/main/java/com/example/shoppingcartserver/appuser/AppUser.java
@@ -0,0 +1,92 @@
+package com.example.shoppingcartserver.appuser;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author aiden
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class AppUser implements UserDetails {
+    //imp UserDetail because we need the security
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked;
+
+    /**
+     * Enable once email confirmed
+     */
+    private Boolean enable;
+
+    public AppUser(String firstName,
+                   String lastName,
+                   String email,
+                   String password,
+                   AppUserRole appUserRole)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = false;
+        this.enable = false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //determine if the user is an admin or a reg user
+        SimpleGrantedAuthority authority
+                = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singleton(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return lastName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enable;
+    }
+}
---
+++ springServer/src/main/java/com/example/shoppingcartserver/appuser/AppUserServiceImpl.java
@@ -0,0 +1,72 @@
+package com.example.shoppingcartserver.appuser;
+
+import com.example.shoppingcartserver.registration.token.ConfirmationToken;
+import com.example.shoppingcartserver.registration.token.ConfirmationTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * @author aiden
+ */
+
+@Service
+@AllArgsConstructor
+public class AppUserServiceImpl implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG,email)));
+    }
+
+    public String signUpUser(AppUser appUser)
+    {
+
+        boolean userExist = appUserRepository
+                .findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if(userExist) {
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        // TODO: Send confirmation token
+
+        //save user
+        appUserRepository.save(appUser);
+
+
+        // send confirm token
+        // create a token then save use the repo
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+                );
+
+        service.saveConfirmationToken(confirmationToken);
+
+        // TODO: Send email to confirm
+
+        return token;
+    }
+
+}
