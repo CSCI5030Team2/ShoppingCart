68c15ba59afc8c968503760af18c09514db2a819
Bohan Zhang <bohan.zhang.1@slu.edu>
2022-10-20 17:43:12 -0500

    SHOP-18 Add: Login module skeleton code

---
+++ springServer/src/test/java/com/example/shoppingcartserver/registration/token/ConfirmationTokenServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.shoppingcartserver.registration.token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfirmationTokenServiceTest {
+
+    ConfirmationTokenService confirmationTokenService;
+
+    @Test
+    void testTest()
+    {
+
+    }
+
+}
