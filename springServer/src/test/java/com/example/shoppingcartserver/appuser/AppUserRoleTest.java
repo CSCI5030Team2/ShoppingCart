e54e9f44dd05b637bbd57f9fc09f702abb6723c7
vponugoti1 <vponugoti1@slu.edu>
2022-11-02 13:22:30 -0500

    App user unit and integration tests using Junit5

---
+++ springServer/src/test/java/com/example/shoppingcartserver/appuser/AppUserRoleTest.java
@@ -0,0 +1,20 @@
+package com.example.shoppingcartserver.appuser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author vivek
+ */
+
+class AppUserRoleTest {
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+}
