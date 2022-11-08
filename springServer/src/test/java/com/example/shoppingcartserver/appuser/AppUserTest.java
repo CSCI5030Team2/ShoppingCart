830d42e0796e81ea5999ba677db14797559ef712
BohanZhang1 <bohan.zhang.1@slu.edu>
2022-11-08 15:32:20 -0600

    Update: test cases

--- springServer/src/test/java/com/example/shoppingcartserver/appuser/AppUserTest.java
+++ springServer/src/test/java/com/example/shoppingcartserver/appuser/AppUserTest.java
@@ -1,103 +1,103 @@
-package com.example.shoppingcartserver.appuser;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * AppUser Test
- * @author vivek
- */
-
-
-class AppUserTest {
-
-    @Test
-    void getAuthorities() {
-
-    }
-
-    @Test
-    void getPassword() {
-    }
-
-    @Test
-    void getUsername() {
-    }
-
-    @Test
-    void isAccountNonExpired() {
-    }
-
-    @Test
-    void isAccountNonLocked() {
-    }
-
-    @Test
-    void isCredentialsNonExpired() {
-    }
-
-    @Test
-    void isEnabled() {
-    }
-
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void getFirstName() {
-    }
-
-    @Test
-    void getLastName() {
-    }
-
-    @Test
-    void getEmail() {
-    }
-
-    @Test
-    void getAppUserRole() {
-    }
-
-    @Test
-    void getLocked() {
-    }
-
-    @Test
-    void getEnable() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void setFirstName() {
-    }
-
-    @Test
-    void setLastName() {
-    }
-
-    @Test
-    void setEmail() {
-    }
-
-    @Test
-    void setPassword() {
-    }
-
-    @Test
-    void setAppUserRole() {
-    }
-
-    @Test
-    void setLocked() {
-    }
-
-    @Test
-    void setEnable() {
-    }
-}
+//package com.example.shoppingcartserver.appuser;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+///**
+// * AppUser Test
+// * @author vivek
+// */
+//
+//
+//class AppUserTest {
+//
+//    @Test
+//    void getAuthorities() {
+//
+//    }
+//
+//    @Test
+//    void getPassword() {
+//    }
+//
+//    @Test
+//    void getUsername() {
+//    }
+//
+//    @Test
+//    void isAccountNonExpired() {
+//    }
+//
+//    @Test
+//    void isAccountNonLocked() {
+//    }
+//
+//    @Test
+//    void isCredentialsNonExpired() {
+//    }
+//
+//    @Test
+//    void isEnabled() {
+//    }
+//
+//    @Test
+//    void getId() {
+//    }
+//
+//    @Test
+//    void getFirstName() {
+//    }
+//
+//    @Test
+//    void getLastName() {
+//    }
+//
+//    @Test
+//    void getEmail() {
+//    }
+//
+//    @Test
+//    void getAppUserRole() {
+//    }
+//
+//    @Test
+//    void getLocked() {
+//    }
+//
+//    @Test
+//    void getEnable() {
+//    }
+//
+//    @Test
+//    void setId() {
+//    }
+//
+//    @Test
+//    void setFirstName() {
+//    }
+//
+//    @Test
+//    void setLastName() {
+//    }
+//
+//    @Test
+//    void setEmail() {
+//    }
+//
+//    @Test
+//    void setPassword() {
+//    }
+//
+//    @Test
+//    void setAppUserRole() {
+//    }
+//
+//    @Test
+//    void setLocked() {
+//    }
+//
+//    @Test
+//    void setEnable() {
+//    }
+//}
