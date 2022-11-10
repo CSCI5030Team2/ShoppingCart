b8827c899a0cb33ba7880b0e5d3e9f79232db3a4
yashb196 <yashkamal.bhatia@slu.edu>
2022-11-10 17:10:55 -0600

    SHOP-4 As a guest user/registered user, I should be able to add item to cart and checkout.

--- client/shopkart/src/App.js
+++ client/shopkart/src/App.js
@@ -6,6 +6,7 @@ import CreateProducts from "./components/CreateProduct";
 import Register from "./components/register"
 import Login from "./components/Login"
 import Cart from "./components/Cart";
+import NavigationAfterLogin from "./components/NavigationAfterLogin" ;
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/register" component={Register}></Route>
         <Route exact path="/login" component={Login}></Route>
         <Route exact path="/cart" component={Cart}></Route>
+        <Route exact path="/navigation" component={NavigationAfterLogin}></Route>
       </div>
     </Router>
   );
--- client/shopkart/src/actions/products.js
+++ client/shopkart/src/actions/products.js
@@ -9,6 +9,7 @@ import {
   import axios from "axios";
   
   export const getProducts = () => dispatch => {
+    console.log(localStorage.getItem("token"));
     return axios
       .get("http://localhost:8080/item")
       .then(res => {
@@ -26,9 +27,9 @@ import {
   export const createProducts = PRODUCTS => dispatch => {
     axios
       .post("http://localhost:8080/admin/item", PRODUCTS,
-    //    {
-    //     headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-    //   }
+       {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+      }
       )
       .then(res => {
         dispatch({
@@ -102,6 +103,7 @@ import {
   };
   
   export const getCarts = () => dispatch => {
+    console.log(localStorage.getItem("token"));
     return axios
       .get("http://localhost:8080/cart")
       .then(res => {
