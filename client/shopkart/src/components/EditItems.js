1f47f4a37bfca27bfd798fd7405ac06ccb12bc88
YMahitha <mahitha.yannamani@slu.edu>
2022-11-30 18:03:59 -0600

    bug fix

--- client/shopkart/src/components/EditItems.js
+++ client/shopkart/src/components/EditItems.js
@@ -1,60 +1,93 @@
-import { Form, Button} from "react-bootstrap"
-import { useState, useContext } from "react"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getProducts, updateProducts } from "../actions/products";
+import { Link } from "react-router-dom";
+import AdminNavbar from "./AdminNavbar";
 
-import {product} from '../actions/products' ;
+export class EditItems extends Component {
+  state = {
+    id: this.props.match.params.productid,
+    item_name: this.props.location.state.product.item_name,
+    price: this.props.location.state.product.price,
+    quantity: this.props.location.state.product.quantity
+  };
 
-const EditItem = ( {theProduct}) => {
-
-    const id = theProduct.id;
-
-    const [name, setName] = useState(theProduct.itemName)
-    const [quantity, setQuantity] = useState(theProduct.quantity)
-    const [price, setPrice] = useState(theProduct.price)
-
-    const {updateItem} = useContext(product);
-
-    const updatedItem = {name, quantity, price}
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        updateItem(name, updatedItem)
+  componentWillMount() {
+    if (!localStorage.getItem("token")) {
+      this.props.history.push("/");
     }
+  }
 
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    console.log(this.props);
     return (
-        <Form>
-            <Form.Group>
-                <Form.Control
-                type = "text"
-                placeholder="Product Name"
-                name="name"
-                value={name}
-                onChange={(e)=> setName(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                type = "text"
-                placeholder="Quantity"
-                name="quantity"
-                value={quantity}
-                onChange={(e)=> setQuantity(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                type = "text"
-                placeholder="Price"
-                name="price"
-                value={price}
-                onChange={(e)=> setPrice(e.target.value)}
-                />
-            </Form.Group>
-            <Button variant="success" type="submit" block>
-                Edit Employee
-            </Button>
-        </Form>
-
-    )
+      <div>
+        <AdminNavbar />
+        <div
+          className="container"
+          style={{ height: 30 + "em", marginTop: 1 + "em" }}
+          align="center"
+        >
+          {/* <form> */}
+          <h1>Update Product</h1>
+          <input
+            type="text"
+            name="name"
+            placeholder=" Product Name"
+            onChange={this.onChange}
+            value={this.state.item_name}
+          />
+          <p>
+            <input
+              type="text"
+              name="price"
+              placeholder=" Product Price"
+              onChange={this.onChange}
+              value={this.state.price}
+            />{" "}
+          </p>
+          <p>
+            <input
+              type="text"
+              name="quantity"
+              placeholder=" Product Quantity"
+              onChange={this.onChange}
+              value={this.state.quantity}
+            />
+          </p>
+          <p>
+            <Link to="/displayproducts">
+              <button
+                onClick={() =>
+                  this.props.updateProducts({
+                    name: this.state.name,
+                    price: this.state.price,
+                    quantity: this.state.quantity,
+                    id: this.state.id
+                  })
+                }
+                onChange={this.onChange}
+              >
+                Update
+              </button>
+            </Link>
+          </p>
+          {/* </form> */}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default EditItem;
+const mapStateToProps = state => ({
+  product: state.productReducer.product
+});
+
+export default connect(
+  mapStateToProps,
+  { getProducts, EditItems }
+)(EditItems);
